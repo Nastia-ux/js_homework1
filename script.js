23ed6af495b5e1844e175946a9dcdf0b96aee334
Zaiets <nastia.zaya1@gmail.com>
2024-02-14 15:41:42 +0200

    DZ2

---
+++ script.js
@@ -0,0 +1,46 @@
+
+var years = prompt("Вкажіть вік: ");{
+
+if (years === '1') {
+    console.log(years + " рік");} 
+    else if (years === '21') {
+        console.log(years + " рік");} 
+        else if  (years === '31') {
+            console.log(years + " рік");} 
+            else if (years === '41') {
+                console.log(years + " рік");} 
+                else if (years === '51') {
+                    console.log(years + " рік");} 
+                    else if (years === '61') {
+                        console.log(years + " рік");} 
+                        else if (years === '71') {
+                            console.log(years + " рік");} 
+                            else if (years === '81') {
+                                console.log(years + " рік");}
+                                else if (years === '91') {
+                                    console.log(years + " рік");}  
+else if (years >= '2' && years <= '4') {
+    console.log(years + " роки");} 
+else if (years >= '22' && years <= '24') {
+    console.log(years + " роки");}
+    else if (years >= 32 && years <= 34) {
+        console.log(years + " роки");}
+        else if (years >= 42 && years <= 44) {
+            console.log(years + " роки");}
+            else if (years >= 52 && years <= 54) {
+                console.log(years + " роки");}
+                else if (years >= 62 && years <= 64) {
+                    console.log(years + " роки");}
+                    else if (years >= 72 && years <= 74) {
+                        console.log(years + " роки");}
+                        else if (years >= 82 && years <= 84) {
+                            console.log(years + " роки");}
+                            else if (years >= 92 && years <= 94) {
+                                console.log(years + " роки");}
+    else if (years > 100) {
+        console.log("Нічого собіб поважний вік :)");}
+        else if (years === "") {
+            console.log("Заповніть будь ласка поле :)");}
+            else   {
+                console.log(years + " років");}
+ }
