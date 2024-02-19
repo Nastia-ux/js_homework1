e6132f186300526cb7875523c3239f47ef48fd9b
Zaiets <nastia.zaya1@gmail.com>
2024-02-19 14:09:25 +0200

    dz - retaking

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
-
-var years = prompt("Вкажіть вік: ");{
-
-if (years === '1') {
-    console.log(years + " рік");} 
-    else if (years === '21') {
-        console.log(years + " рік");} 
-        else if  (years === '31') {
-            console.log(years + " рік");} 
-            else if (years === '41') {
-                console.log(years + " рік");} 
-                else if (years === '51') {
-                    console.log(years + " рік");} 
-                    else if (years === '61') {
-                        console.log(years + " рік");} 
-                        else if (years === '71') {
-                            console.log(years + " рік");} 
-                            else if (years === '81') {
-                                console.log(years + " рік");}
-                                else if (years === '91') {
-                                    console.log(years + " рік");}  
-else if (years >= '2' && years <= '4') {
-    console.log(years + " роки");} 
-else if (years >= '22' && years <= '24') {
-    console.log(years + " роки");}
-    else if (years >= 32 && years <= 34) {
-        console.log(years + " роки");}
-        else if (years >= 42 && years <= 44) {
-            console.log(years + " роки");}
-            else if (years >= 52 && years <= 54) {
-                console.log(years + " роки");}
-                else if (years >= 62 && years <= 64) {
-                    console.log(years + " роки");}
-                    else if (years >= 72 && years <= 74) {
-                        console.log(years + " роки");}
-                        else if (years >= 82 && years <= 84) {
-                            console.log(years + " роки");}
-                            else if (years >= 92 && years <= 94) {
-                                console.log(years + " роки");}
-    else if (years > 100) {
-        console.log("Нічого собі, поважний вік :)");}
-
-        else if (years === "") {
-            console.log("Заповніть будь ласка поле :)");}
-            
-            else   {
-                console.log(years + " років");}
- }
+var years = prompt("Вкажіть вік: ");
+ 
+if (years < 1 || isNaN(years)) {
+    alert("Введено некоректне число. Спробуйте знову.");
+} else {
+  years % 100; 
+    if (years == 1 || (years % 10 == 1 && years != 11)) {
+      console.log(years + " рік");
+    } else if ((years >= 2 && years <= 4) || (years % 10 >= 2 && years % 10 <= 4 && (years < 10 || years > 20))) {
+      console.log(years + " роки");
+    } else {
+      console.log(years + " років");
+    }
+}
